7f56f05741d1100f582a1f14bb3b47c3c1fbbb58
Bhavya <bbreddy@mcdonogh.org>
2025-04-03 11:43:46 -0400

    delete readin class

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
         
@@ -6,6 +9,7 @@ public class Main {
     }
     //method to read in file 
     public static void readFile(){
+        File inputFile = new File("murdle.txt");
         
     }
 }
