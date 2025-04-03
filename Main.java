70930ea24b1c00752c0d63f0aef8971badf5863a
Bhavya <bbreddy@mcdonogh.org>
2025-04-03 11:45:03 -0400

    changed code

--- Main.java
+++ Main.java
@@ -7,9 +7,19 @@ public class Main {
         
 
     }
+    
     //method to read in file 
     public static void readFile(){
         File inputFile = new File("murdle.txt");
-        
+        try {
+			//Scans all words as long as there is a word next
+			Scanner in = new Scanner(inputFile);
+			while(in.hasNextLine()){
+				//Add in other stuff
+			}
+				
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
     }
 }
