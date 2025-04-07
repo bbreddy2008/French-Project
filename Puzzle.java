b8284b3c1a7a2266d76accd9e2681df2c46cdeac
Ryleigh <rmmccomas@mcdonogh.org>
2025-04-07 08:44:24 -0400

    added arraylists

--- Puzzle.java
+++ Puzzle.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 public class Puzzle {
     private int id;
     //arrayLists to hold each type of answer
-    private ArrayList<String> qui = new ArrayList<String>(); 
-    
+    private ArrayList<String> qui = new ArrayList<String>();
+    private ArrayList<String> quoi= new ArrayList<String>();
+    private ArrayList<String> oui = new ArrayList<String>();
+    private ArrayList<String> pourquoi = new ArrayList<String>(); 
 }
