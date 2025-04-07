6a466144a622bf71713c6ddbdb7913f1495630df
Haydn <HMitchell@McDonogh.org>
2025-04-07 09:23:29 -0400

    Added all Add functions to Input

--- Main.java
+++ Main.java
@@ -15,18 +15,31 @@ public class Main {
 			int counter = 1;
 			while(in.hasNextLine()){
 				if(counter >= 50){
+
+					String[][] storedValue = new String[3][3];
 					String lineA = in.nextLine();
 					String lineB = in.nextLine();
 					String lineC = in.nextLine();
 					String[] splitLineA = lineA.split(",");
 					String[] splitLineB = lineB.split(",");
 					String[] splitLineC = lineC.split(",");
-					Puzzle puzz = new Puzzle();//add counter
-					puzz.addQui(splitLineA[1]);
-					puzz.addQuoi(splitLineA[2]);
-					puzz.addOu(splitLineA[3]);
+
+					for(int i = 0; i < storedValue.length; i++){
+						storedValue[0][i] = splitLineA[i + 1];
+						storedValue[1][i] = splitLineB[i];
+						storedValue[2][i] = splitLineC[i];
+					}
+
+					Puzzle puzz = new Puzzle(counter);
+					for(int i = 0; i < storedValue.length; i++){
+						puzz.addQui(storedValue[i][0]);
+						puzz.addQuoi(storedValue[i][1]);
+						puzz.addOu(storedValue[i][2]);
+					}
 
 				}else{
+
+					String[][] storedValue = new String[4][4];
 					String lineA = in.nextLine();
 					String lineB = in.nextLine();
 					String lineC = in.nextLine();
@@ -35,6 +48,22 @@ public class Main {
 					String[] splitLineB = lineB.split(",");
 					String[] splitLineC = lineC.split(",");
 					String[] splitLineD = lineD.split(",");
+
+					for(int i = 0; i < storedValue.length; i++){
+						storedValue[0][i] = splitLineA[i + 1];
+						storedValue[1][i] = splitLineB[i];
+						storedValue[2][i] = splitLineC[i];
+						storedValue[3][i] = splitLineD[i];
+					}
+
+					Puzzle puzz = new Puzzle(counter);
+					for(int i = 0; i < storedValue.length; i++){
+						puzz.addQui(storedValue[i][0]);
+						puzz.addQuoi(storedValue[i][1]);
+						puzz.addOu(storedValue[i][2]);
+						puzz.addPoruoi(storedValue[i][3]);
+					}
+
 				}
 			}
 			in.close();
