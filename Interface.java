688ac50d385935276172ec71e64053d3a4c0b819
Turtsle <94251345+Turtsle@users.noreply.github.com>
2025-04-08 19:23:47 -0400

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -22,7 +22,6 @@ public class Interface extends JPanel {
     JLabel porquoiLabel; // puzzle number labels
     
     public Interface() {
-<<<<<<< HEAD
         Font font;
         try { // initalize the font we're using
             font = Font.createFont(Font.TRUETYPE_FONT, new File("Gluten-VariableFont_slnt,wght.ttf")).deriveFont(Font.BOLD,40f);
@@ -38,24 +37,6 @@ public class Interface extends JPanel {
         }
     
         //add logo
-=======
-        Font font = null;
-        try {
-            font = Font.createFont(Font.TRUETYPE_FONT, new File("Gluten-VariableFont_slnt,wght.ttf"));
-        } catch (FontFormatException | IOException exception) {
-            font = new Font("Arial", Font.PLAIN, 12);
-        }
-
-        setLayout(null);
-        try {                
-            logo = ImageIO.read(this.getClass().getResource("logo.png"));
-        } catch (IOException ex) {
-            // do nothing
-        }
-
-    
-        //logo
->>>>>>> refs/remotes/origin/main
         logoPanel = new JLabel(new ImageIcon(logo));
         logoPanel.setBounds(0, 0, 1500, 250);
         add(logoPanel);
@@ -63,7 +44,6 @@ public class Interface extends JPanel {
         // puzzle number
         puzzleNumber = new JTextField();
         puzzleNumber.setColumns(2);
-<<<<<<< HEAD
         puzzleNumber.setBounds(220, 205, 70, 50);
         puzzleNumber.setFont(font);
         puzzleNumber.setOpaque(false);
@@ -129,42 +109,6 @@ public class Interface extends JPanel {
         porquoiLabel.setFont(font.deriveFont(Font.BOLD, 50f));
         porquoiLabel.setBounds(150,585,1000,100);
         add(porquoiLabel);
-=======
-        puzzleNumber.setBounds(220, 200, 80, 60);
-        puzzleNumber.setFont(font);
-        add(puzzleNumber);
-
-        puzzleLabel = new JLabel("Puzzle:");
-        puzzleLabel.setFont(font.deriveFont(Font.BOLD, 50f));
-        puzzleLabel.setBounds(50,180,1000,100);
-        add(puzzleLabel);
-
-        // all text fields
-        quiInputField = new JTextField();
-        quiInputField.setColumns(2);
-        quiInputField.setFont(font);
-        quiInputField.setBounds(450, 300, 600, 60);
-        add(quiInputField);
-
-        quoiInputField = new JTextField();
-        quoiInputField.setColumns(2);
-        quoiInputField.setFont(font);
-        quoiInputField.setBounds(450, 400, 600, 60);
-        add(quoiInputField);
-
-        ouInputField = new JTextField();
-        ouInputField.setColumns(2);
-        ouInputField.setFont(font);
-        ouInputField.setBounds(450, 400, 600, 60);
-        add(ouInputField);
-
-        pourquoiInputField = new JTextField();
-        pourquoiInputField.setColumns(2);
-        pourquoiInputField.setFont(font);
-        pourquoiInputField.setBounds(450, 400, 600, 60);
-        add(pourquoiInputField);
-
->>>>>>> refs/remotes/origin/main
 
         // window stuff
         setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
@@ -192,7 +136,6 @@ public class Interface extends JPanel {
          g2.setColor(new Color(255, 247, 232));
          g2.fillRect(0, 0, getWidth(), getHeight());
 
-<<<<<<< HEAD
          // roundeded text boxes
          g2.setStroke(new BasicStroke(2));
          drawRoundedBox(g2, puzzleNumber);
@@ -215,13 +158,5 @@ public class Interface extends JPanel {
     
         g.setColor(Color.GRAY);
         g.drawRoundRect(x, y, width, height, degrees, degrees);
-=======
-         // roundeded text box puzzle input
-
-         // g2.setStroke(new java.awt.BasicStroke(5)); // thickness of 3.0f
-         // g2.setColor(Color.black);
-         // RoundRectangle2D roundedRectangle = new RoundRectangle2D.Double(200, 100, 100, 100, 15, 15);
-         // g2.draw(roundedRectangle);
->>>>>>> refs/remotes/origin/main
     }
 }
