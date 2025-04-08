b49aaee8710be31bdfaf950a29d0d9a36b214386
Haydn <HMitchell@McDonogh.org>
2025-04-08 15:19:38 -0400

    Finalizing base

--- Interface.java
+++ Interface.java
@@ -1,38 +1,85 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
 
 
-
 public class Interface extends JPanel {
 
     private BufferedImage logo; // logo
     JLabel logoPanel; // logo panel
-    JTextField quiTextField; // quiTextField
+    JTextField puzzleNumber; // quiTextField
+    JLabel puzzleLabel; // puzzle number label
+    JTextField quiInputField; // quiTextField
+    JLabel quiLabel; // puzzle number label
+    JTextField quoiInputField; // quiTextField
+    JLabel quoiLabel; // puzzle number label
+    JTextField ouInputField; // quiTextField
+    JLabel ouLabel; // puzzle number label
+    JTextField pourquoiInputField; // quiTextField
+    JLabel porquoiLabel; // puzzle number labels
     
     public Interface() {
-        setLayout(null);
-        try {        
-            logo = ImageIO.read(this.getClass().getResource("logo.png"));
-        } catch (IOException ex) {
-            // ¯\_(ツ)_/¯
+        Font font = null;
+        try {
+            font = Font.createFont(Font.TRUETYPE_FONT, new File("Gluten-VariableFont_slnt,wght.ttf"));
+        } catch (FontFormatException | IOException exception) {
+            font = new Font("Arial", Font.PLAIN, 12);
         }
 
+        setLayout(null);
+        try {                
+            logo = ImageIO.read(this.getClass().getResource("logo.png"));
+        } catch (IOException ex) {
+            // do nothing
+        }
+
+    
         //logo
         logoPanel = new JLabel(new ImageIcon(logo));
         logoPanel.setBounds(0, 0, 1500, 250);
         add(logoPanel);
-
+        
         // puzzle number
+        puzzleNumber = new JTextField();
+        puzzleNumber.setColumns(2);
+        puzzleNumber.setBounds(220, 200, 80, 60);
+        puzzleNumber.setFont(font);
+        add(puzzleNumber);
 
-        quiTextField = new JTextField();
-        quiTextField.setColumns(2);
-        Font bigFont = quiTextField.getFont().deriveFont(Font.PLAIN, 50f);
-        quiTextField.setFont(bigFont);
-        add(quiTextField);
+        puzzleLabel = new JLabel("Puzzle:");
+        puzzleLabel.setFont(font.deriveFont(Font.BOLD, 50f));
+        puzzleLabel.setBounds(50,180,1000,100);
+        add(puzzleLabel);
+
+        // all text fields
+        quiInputField = new JTextField();
+        quiInputField.setColumns(2);
+        quiInputField.setFont(font);
+        quiInputField.setBounds(450, 300, 600, 60);
+        add(quiInputField);
+
+        quoiInputField = new JTextField();
+        quoiInputField.setColumns(2);
+        quoiInputField.setFont(font);
+        quoiInputField.setBounds(450, 400, 600, 60);
+        add(quoiInputField);
+
+        ouInputField = new JTextField();
+        ouInputField.setColumns(2);
+        ouInputField.setFont(font);
+        ouInputField.setBounds(450, 400, 600, 60);
+        add(ouInputField);
+
+        pourquoiInputField = new JTextField();
+        pourquoiInputField.setColumns(2);
+        pourquoiInputField.setFont(font);
+        pourquoiInputField.setBounds(450, 400, 600, 60);
+        add(pourquoiInputField);
+
 
         // window stuff
         setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
@@ -50,7 +97,19 @@ public class Interface extends JPanel {
     }
 
     public void paintComponent(Graphics g) {
-        g.setColor(new Color(255, 247, 232));
-        g.fillRect(0, 0, getWidth(), getHeight());
+         java.awt.Graphics2D g2 = (java.awt.Graphics2D) g.create();
+         Graphics2D g2d = (Graphics2D) g;
+
+         // background
+
+         g2.setColor(new Color(255, 247, 232));
+         g2.fillRect(0, 0, getWidth(), getHeight());
+
+         // roundeded text box puzzle input
+
+         // g2.setStroke(new java.awt.BasicStroke(5)); // thickness of 3.0f
+         // g2.setColor(Color.black);
+         // RoundRectangle2D roundedRectangle = new RoundRectangle2D.Double(200, 100, 100, 100, 15, 15);
+         // g2.draw(roundedRectangle);
     }
 }
--- Main.java
+++ Main.java
@@ -2,8 +2,18 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	private static Puzzle[] puzzles;
+	private static Scanner scanner = new Scanner(System.in);
+	private static String qui_answer;
+    private static String quoi_answer;
+    private static String ou_answer;
+    private static String pourquoi_answer;
     public static void main(String[] args) {
+		puzzles = new Puzzle[100];
 		readFile();
+		System.out.println("Enter the id of puzzle.");
+		int n = scanner.nextInt();
+		getInput(n);
 	}
     
     //method to read in file 
@@ -12,14 +22,18 @@ public class Main {
         try {
 			//Scans all words as long as there is a word next
 			Scanner in = new Scanner(inputFile);
+			String skip = in.nextLine();
 			int counter = 1;
-			while(in.hasNextLine()){
-				if(counter >= 50){
+			while (counter <= 100){
+				if (counter <= 50){
 
 					String[][] storedValue = new String[3][3];
 					String lineA = in.nextLine();
+					System.out.println(lineA);
 					String lineB = in.nextLine();
+					System.out.println(lineB);
 					String lineC = in.nextLine();
+					System.out.println(lineC);
 					String[] splitLineA = lineA.split(",");
 					String[] splitLineB = lineB.split(",");
 					String[] splitLineC = lineC.split(",");
@@ -30,20 +44,21 @@ public class Main {
 						storedValue[2][i] = splitLineC[i];
 					}
 
-					Puzzle puzz = new Puzzle(counter);
-					for(int i = 0; i < storedValue.length; i++){
-						puzz.addQui(storedValue[i][0]);
-						puzz.addQuoi(storedValue[i][1]);
-						puzz.addOu(storedValue[i][2]);
-					}
+					Puzzle puzz = new Puzzle(counter, storedValue);
+					puzzles[counter-1] = puzz;
+					counter++;
 
-				}else{
+				} else {
 
 					String[][] storedValue = new String[4][4];
 					String lineA = in.nextLine();
+					System.out.println(lineA);
 					String lineB = in.nextLine();
+					System.out.println(lineB);
 					String lineC = in.nextLine();
+					System.out.println(lineC);
 					String lineD = in.nextLine();
+					System.out.println(lineD);
 					String[] splitLineA = lineA.split(",");
 					String[] splitLineB = lineB.split(",");
 					String[] splitLineC = lineC.split(",");
@@ -56,13 +71,9 @@ public class Main {
 						storedValue[3][i] = splitLineD[i];
 					}
 
-					Puzzle puzz = new Puzzle(counter);
-					for(int i = 0; i < storedValue.length; i++){
-						puzz.addQui(storedValue[i][0]);
-						puzz.addQuoi(storedValue[i][1]);
-						puzz.addOu(storedValue[i][2]);
-						puzz.addPoruoi(storedValue[i][3]);
-					}
+					Puzzle puzz = new Puzzle(counter, storedValue);
+					puzzles[counter-1] = puzz;
+					counter++;
 
 				}
 			}
@@ -71,5 +82,20 @@ public class Main {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+    }
+
+	//Input from the interface
+    public static void getInput(int id){
+        if (id <= 50){
+            qui_answer= scanner.nextLine();
+            quoi_answer= scanner.nextLine();
+            ou_answer= scanner.nextLine();
+            pourquoi_answer = null;
+        } else {
+            qui_answer= scanner.nextLine();
+            quoi_answer= scanner.nextLine();
+            ou_answer= scanner.nextLine();
+            pourquoi_answer = scanner.nextLine();
+        }
     }
 }
