98db1b26287f679b5bda9aa48d9f36d2e047043d
Haydn <HMitchell@McDonogh.org>
2025-04-14 11:06:35 -0400

    Co-authored-by: bbreddy2008 <bbreddy2008@users.noreply.github.com> Co-authored-by: Ryleigh McComas  <ryleighmccomas@users.noreply.github.com>

--- Main.java
+++ Main.java
@@ -4,34 +4,61 @@ import java.awt.*;
 
 //The main program that activates all the programs
 public class Main {
+
+	//The array of puzzle objects that hold the andswers
 	private static Puzzle[] puzzles;
+
+	//The interface of the program that adds usability
 	private static Interface myInterface;
 	private static Scanner scanner = new Scanner(System.in);
+
+	//Begins the program
     public static void main(String[] args) {
+
+		//Creates all of the puzzle objects and reads in the txt file with the answers
 		puzzles = new Puzzle[100];
 		readFile();
+
+		//Creates the interface
 		myInterface = new Interface();
 		
 	}
 
 		//Returned to Interface. Return values determine Green or Red coloring for rounded boxes.
 	public static boolean[] checkAnswers(){
+
+		//Array of booleans to determine which input is true
 		boolean[] yOrN;
+
+		//Gets the current puzzle
 		int pez = myInterface.getPuzzle() - 1;
+
+		//If the current puzzle is 50 or lower, it checks for 3 answers
 		if (pez <= 50){
 			yOrN = new boolean[3];
+
 			for (int i = 0; i < 3; i++) {
+
+				//If the answer in the input box is the same as the answer stored in the puzzle object, Return True
 				if (myInterface.userAnswers()[i].equalsIgnoreCase(puzzles[pez].getCorrect()[i])) {
 					yOrN[i] = true;
+
+					//Otherwise return false
 				} else {
 					yOrN[i] = false;
 				}
 			}
+		//If the current puzzle is 51 or higher, it checks for 4 answers
 		} else {
 			yOrN = new boolean[4];
+
 			for (int i = 0; i < 4; i++) {
+
+				//If the answer in the input box is the same as the answer stored in the puzzle object, Return True
 				if (myInterface.userAnswers()[i].equalsIgnoreCase(puzzles[pez].getCorrect()[i])) {
 					yOrN[i] = true;
+
+					//Otherwise return false
 				} else {
 					yOrN[i] = false;
 				}
@@ -47,41 +74,61 @@ public class Main {
         try {
 			//Scans all words as long as there is a word next
 			Scanner in = new Scanner(inputFile);
+
+			//Skips the first line since its just allignment information
 			String skip = in.nextLine();
+
+			//Used to determine puzzle number
 			int counter = 1;
+
+			//While below 100 puzzles
 			while (counter <= 100){
+
+				//For puzzle number 50 or lower
 				if (counter <= 50){
 
+					//Creates, reads, and splits up a 3 by 3 2D array with answers at the top
 					String[][] storedValue = new String[3][3];
+
 					String lineA = in.nextLine();
 					String lineB = in.nextLine();
 					String lineC = in.nextLine();
+
 					String[] splitLineA = lineA.split(",");
 					String[] splitLineB = lineB.split(",");
 					String[] splitLineC = lineC.split(",");
 
+					//Stores the inputs into the 2d Array
 					for(int i = 0; i < storedValue.length; i++){
 						storedValue[0][i] = splitLineA[i + 1];
 						storedValue[1][i] = splitLineB[i];
 						storedValue[2][i] = splitLineC[i];
 					}
 
+					//Sends the input 2D array and the puzzle number to make a new puzzle, then stores it in the array
 					Puzzle puzz = new Puzzle(counter, storedValue);
 					puzzles[counter-1] = puzz;
+
+					//increases the counter to indicate new puzzle number
 					counter++;
 
+				//For puzzle numbers 51 and higher
 				} else {
 
+					//Creates, reads, and splits up a 4 by 4 2D array with answers at the top
 					String[][] storedValue = new String[4][4];
+
 					String lineA = in.nextLine();
 					String lineB = in.nextLine();
 					String lineC = in.nextLine();
 					String lineD = in.nextLine();
+
 					String[] splitLineA = lineA.split(",");
 					String[] splitLineB = lineB.split(",");
 					String[] splitLineC = lineC.split(",");
 					String[] splitLineD = lineD.split(",");
 
+					//Stores the inputs into the 2d Array
 					for(int i = 0; i < storedValue.length; i++){
 						storedValue[0][i] = splitLineA[i + 1];
 						storedValue[1][i] = splitLineB[i];
@@ -89,12 +136,16 @@ public class Main {
 						storedValue[3][i] = splitLineD[i];
 					}
 
+					//Sends the input 2D array and the puzzle number to make a new puzzle, then stores it in the array			
 					Puzzle puzz = new Puzzle(counter, storedValue);
 					puzzles[counter-1] = puzz;
+
+					//increases the counter to indicate new puzzle number
 					counter++;
 
 				}
 			}
+			//Throws an error without this
 			in.close();
 				
 		} catch (FileNotFoundException e) {
@@ -102,11 +153,13 @@ public class Main {
 		}
     }
 
+	//If the Interface submit button is cliced, then it activates check answers
 	public static boolean[] submitButtonClicked() {
 		System.out.println("Submit button clicked!");
 		return checkAnswers();
 	}
 
+	//Returns current puzzle data
 	public static Puzzle givePuzzleData(){
 		return puzzles[myInterface.getPuzzle()];
 	}
--- Puzzle.java
+++ Puzzle.java
@@ -4,37 +4,43 @@ import java.util.*;
 public class Puzzle {
     //puzzle id number
     private int id;
+    
     //Strings of each asnwer type 
     private String qui_answer;
     private String quoi_answer;
     private String ou_answer;
     private String pourquoi_answer;
+    
     //arrays to hold each type of answer
     private String[] qui;
     private String[] quoi;
     private String[] ou;
     private String[] pourquoi;
 
-    //constructor 
+    //constructor for a puzzle 
     public Puzzle(int numb, String[][] arrays){  
         id = numb;
-        if (id <= 50){
+        //if the id number is less than 50...
+        if (id <= 50){ // creates arrays of size 3
             qui = new String[3];
             quoi = new String[3];
             ou = new String[3];
 
+            // fills array with answers
             for (int i = 0; i < 3; i++){
                 qui[i] = arrays[i][0];
                 quoi[i] = arrays[i][1];
                 ou[i] = arrays[i][2];
             }
 
-        } else {
+        } else { //if the id number is 51 and above 
+            // creates array of size 4
             qui = new String[4];
             quoi = new String[4];
             ou = new String[4];
             pourquoi = new String[4];
 
+            // fills array with answers
             for (int i = 0; i < 4; i++){
                 qui[i] = arrays[i][0];
                 quoi[i] = arrays[i][1];
@@ -46,13 +52,13 @@ public class Puzzle {
         }
     }
 
-    //methods
+    //returns puzzle id
     public int puzzleID(){
         return id;
     }
 
-    //Only the first value
-    // puzzles[5].getCorrect()
+    // Only the first value
+    // returns the correct answers for checking
     public String[] getCorrect(){
         if (id <= 50){
             String[] send = {qui[0], quoi[0], ou[0]};
@@ -63,7 +69,7 @@ public class Puzzle {
         }
     }
 
-    //All options
+    //Accesses all of the possible option answers for the puzzle 
     public String[] getOptions(String[] array){
         String[] options = new String[array.length];
         for (int i = 0; i < array.length; i++) {
@@ -72,6 +78,7 @@ public class Puzzle {
         return options;
     }
 
+    //prints all the correct puzzle answers; all located at index 1
     public String print(){
         return qui[1] + " " + quoi[1] + " " + ou[1] + " " + pourquoi[1];
     }
